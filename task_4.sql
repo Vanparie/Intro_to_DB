ee556308043925c7d88172c096e34f4e33758e7e
Davidson Leparie <davidsonleparie@gmail.com>
2024-10-20 01:13:11 +0300

    Added an sql file to the database

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,14 @@
-USE alx_book_store;
+-- Query to retrieve the full description of the books table
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_DEFAULT, 
+    COLUMN_KEY, 
+    EXTRA 
+FROM 
+    information_schema.COLUMNS 
+WHERE 
+    TABLE_NAME = 'books' 
+    AND TABLE_SCHEMA = 'alx_book_store';
 
-SELECT * FROM Books;
