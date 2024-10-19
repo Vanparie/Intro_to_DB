f1be32c90d9dc66a5bd9bf077e9e477eaf7138b1
Davidson Leparie <davidsonleparie@gmail.com>
2024-10-20 01:15:36 +0300

    Added an sql file to the database

--- task_4.sql
+++ task_4.sql
@@ -7,8 +7,8 @@ SELECT
     COLUMN_KEY, 
     EXTRA 
 FROM 
-    information_schema.COLUMNS 
+    INFORMATION_SCHEMA.COLUMNS 
 WHERE 
-    TABLE_NAME = 'books' 
+    TABLE_NAME = 'Books' 
     AND TABLE_SCHEMA = 'alx_book_store';
 
